9e006beb5ed4285a538a3a82d56214b8a38258ad
Benjamin Noufel <benjamin.noufel@gmail.com>
2021-04-12 14:45:17 +0200

    :boom: Change appendScript and appendStylesheet props

--- src/dom.ts
+++ src/dom.ts
@@ -1,60 +1,73 @@
 import {logger} from "./utils";
 
+interface AppendScriptAttributes extends Partial<HTMLScriptElement> {
+    src: string;
+    async: boolean;
+}
+
+interface AppendStylesheetAttributes extends Partial<HTMLLinkElement> {
+    href: string;
+    rel: string;
+}
+
 /**
  * Append a style on DOM
- * @param {string} href
+ * @param {AppendStylesheetAttributes} attr
  * @returns {Promise<string | void>}
  */
-export const appendStylesheet = (href: string): Promise<string | void> => new Promise((resolve: (value: void) => void): void => {
-    if (!href) {
+export const appendStylesheet = (attr: AppendStylesheetAttributes): Promise<string | void> => new Promise((resolve: (value: void) => void): void => {
+    if (!attr.href) {
         throw new Error("href is required");
     }
-    if ("string" !== typeof href) {
+    if ("string" !== typeof attr.href) {
         throw new TypeError("href must be a string");
     }
-    if (document.querySelector(`link[rel="stylesheet"][href="${href}"]`)) {
+    if (document.querySelector(`link[rel="${attr.rel}"][href="${attr.href}"]`)) {
         resolve();
         return;
     }
     const stylesheet: HTMLElement = document.createElement("link");
-
-    stylesheet.setAttribute("rel", "stylesheet");
-    stylesheet.setAttribute("href", href);
+    const attributes = Object.entries(attr);
+    attributes.forEach(attribute => {
+        stylesheet.setAttribute(attribute[0], attribute[1]);
+    });
     stylesheet.addEventListener("load", () => {
-        logger(`Loaded stylesheet: ${href}`);
+        logger(`Loaded stylesheet: ${attr.href}`);
         resolve();
     });
     document.head.appendChild(stylesheet);
 });
 
 /**
- * Append a script on DOM
- * @param {string} src
- * @returns {Promise<void>}
+ * Append a script to the dom
+ * @param {HTMLScriptElement} attr
+ * @returns {Promise<void | string>}
  */
-export const appendScript = (src: string): Promise<void> => new Promise((resolve: (value: void) => void): void => {
-    if (!src) {
+export const appendScript = (attr: AppendScriptAttributes): Promise<void | string> => new Promise((resolve: (value: void) => void): void => {
+    if (!attr.src) {
         throw new Error("src is required");
     }
-    if ("string" !== typeof src) {
+    if ("string" !== typeof attr.src) {
         throw new TypeError("src must be a string");
     }
 
-    if (document.querySelector(`script[src="${src}"]`)) {
-        logger(`Loaded script: ${src}`);
+    if (document.querySelector(`script[src="${attr.src}"]`)) {
+        logger(`Loaded script: ${attr.src}`);
 
         resolve();
         return;
     }
 
     const script: HTMLScriptElement = document.createElement("script");
-
-    script.setAttribute("src", src);
-    script.setAttribute("async", "");
+    const attributes = Object.entries(attr);
+    attributes.forEach(attribute => {
+        script.setAttribute(attribute[0], attribute[1]);
+    });
 
     document.head.appendChild(script);
 
     script.addEventListener("load", (): void => {
+        logger(`Loaded script: ${attr.src}`);
         resolve();
     });
 });
